7f03aee7dd1f8133d7aa5c79e9fdc225049abc3f
MatomeSelete <seletematome3@gmail.com>
2023-10-17 16:38:11 +0200

    able to fetch multiple work history

--- src/Employment.tsx
+++ src/Employment.tsx
@@ -1,10 +1,6 @@
+
 import { FormWrapper } from "./Formwrapper";
-import React, { useState } from "react"; // Import React and useState
-
-type  EmploymentData = {
-  workHistory: WorkHistoryItem[];
-};
-
+import React, { useState } from "react";
 
 type WorkHistoryItem = {
   company: string;
@@ -13,73 +9,191 @@ type WorkHistoryItem = {
   responsibilities: string;
 };
 
-type EmploymentFormProps = EmploymentData & {
-    updateFields: (fields: Partial<EmploymentData>) => void;
-  };
+type EmploymentData = {
+  workHistory: WorkHistoryItem[];
+};
 
+type EmploymentFormProps = EmploymentData & {
+  updateFields: (fields: Partial<EmploymentData>) => void;
+};
 
 export function EmploymentForm({
-    workHistory: [firstJob],
+  workHistory,
   updateFields,
 }: EmploymentFormProps) {
-  const [company, setCompany] = useState(firstJob?.company || "");
-  const [position, setPosition] = useState(firstJob?.position || "");
-  const [term, setTerm] = useState(firstJob?.term || "");
-  const [responsibilities, setResponsibilities] = useState(firstJob?.responsibilities || "");
+  function addWorkHistory() {
+    const newWorkHistoryItem = {
+      company: "",
+      position: "",
+      term: "",
+      responsibilities: "",
+    };
+    updateFields({
+      workHistory: [...workHistory, newWorkHistoryItem],
+    });
+  }
 
+  function updateWorkHistoryField(index: number, field: string, value: string) {
+    const updatedWorkHistory = [...workHistory];
+    updatedWorkHistory[index] = {
+      ...updatedWorkHistory[index],
+      [field]: value,
+    };
+    updateFields({
+      workHistory: updatedWorkHistory,
+    });
+  }
 
-  // Define an updateFields function specifically for this component
-  function updateEmploymentFields(fields: Partial<EmploymentData>) {
-  updateFields({
-    workHistory: [{ ...firstJob, ...fields }],
-  });
-}
-
-
-    return (
-        <>
-        <FormWrapper title="Employment history">
+  return (
+    <>
+      <FormWrapper title="Employment history">
+        {workHistory.map((workHistoryItem, index) => (
+          <div key={index}>
             <label> Company name </label>
             <input
-                autoFocus
-                required
-                type='text'
-                value={company}
-                // onChange={e => updateFields({ company: e.target.value })} 
-                onChange={(e) => setCompany(e.target.value)} // Update the state using setCompany
-                />
+              autoFocus
+              required
+              type="text"
+              value={workHistoryItem.company}
+              onChange={(e) => updateWorkHistoryField(index, "company", e.target.value)}
+            />
             <label> Position </label>
             <input
-                required
-                type='text'
-                value={position}
-                // onChange={e => updateFields({ position: e.target.value })} 
-                onChange={(e) => setPosition(e.target.value)} // Update the state using setPosition
+              required
+              type="text"
+              value={workHistoryItem.position}
+              onChange={(e) => updateWorkHistoryField(index, "position", e.target.value)}
             />
             <label> Term of Employment </label>
             <input
-                required min={1}
-                type='text'
-                value={term}
-                // onChange={e => updateFields({ term: e.target.value })} 
-                onChange={(e) => setTerm(e.target.value)} // Update the state using setTerm
+              required
+              type="text"
+              value={workHistoryItem.term}
+              onChange={(e) => updateWorkHistoryField(index, "term", e.target.value)}
             />
             <label> Key Responsibilities </label>
             <input
-                required min={1}
-                type='text'
-                value={responsibilities}
-                // onChange={e => updateFields({ responsibilities: e.target.value })}
-                onChange={(e) => setResponsibilities(e.target.value)} // Update the state using setResponsibilities
+              required
+              type="text"
+              value={workHistoryItem.responsibilities}
+              onChange={(e) => updateWorkHistoryField(index, "responsibilities", e.target.value)}
             />
+          </div>
+        ))}
 
-            {/* <button> Add more </button> */}
-
-
-
-        </FormWrapper>
-        </>
-    )
+        <button onClick={addWorkHistory}>Add Work History</button>
+      </FormWrapper>
+    </>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { FormWrapper } from "./Formwrapper";
+// import React, { useState } from "react"; // Import React and useState
+// import { data } from "jquery";
+
+// type  EmploymentData = {
+//   workHistory: WorkHistoryItem[];
+// };
+
+
+// type WorkHistoryItem = {
+//   company: string;
+//   position: string;
+//   term: string;
+//   responsibilities: string;
+// };
+
+// type EmploymentFormProps = EmploymentData & {
+//     updateFields: (fields: Partial<EmploymentData>) => void;
+//   };
+
+
+// export function EmploymentForm({
+//     workHistory: [firstJob],
+//   updateFields,
+// }: EmploymentFormProps) {
+//   const [company, setCompany] = useState(firstJob?.company || "");
+//   const [position, setPosition] = useState(firstJob?.position || "");
+//   const [term, setTerm] = useState(firstJob?.term || "");
+//   const [responsibilities, setResponsibilities] = useState(firstJob?.responsibilities || "");
+
+
+//   // Define an updateFields function specifically for this component
+//   function updateEmploymentFields(fields: Partial<EmploymentData>) {
+//   updateFields({
+//     workHistory: [{ ...firstJob, ...fields }],
+//   });
+// }
+
+// function addWorkHistory() {
+//   const newWorkHistoryItem = {
+//     company: "",
+//     position: "",
+//     term: "",
+//     responsibilities: "",
+//   };
+//   updateFields({
+//     workHistory: [...data.workHistory, newWorkHistoryItem],
+//   });
+// }
+
+
+//     return (
+//         <>
+//         <FormWrapper title="Employment history">
+//         {data.workHistory.map((workHistoryItem, index) => (
+//           <div key={index}>
+//             <label> Company name </label>
+//             <input
+//                 autoFocus
+//                 required
+//                 type='text'
+//                 value={workHistoryItem.company}
+//                 onChange={(e) => updateWorkHistoryField(index, "company", e.target.value)}
+//                 />
+//             <label> Position </label>
+//             <input
+//                 required
+//                 type='text'
+//                 value={workHistoryItem.position}
+//                 onChange={(e) => updateWorkHistoryField(index, "position", e.target.value)}
+//             />
+//             <label> Term of Employment </label>
+//             <input
+//                 required min={1}
+//                 type='text'
+//                 value={workHistoryItem.term}
+//                 onChange={(e) => updateWorkHistoryField(index, "term", e.target.value)}
+//             />
+//             <label> Key Responsibilities </label>
+//             <input
+//                 required min={1}
+//                 type='text'
+//                 value={workHistoryItem.responsibilities}
+//                 onChange={(e) => updateWorkHistoryField(index, "responsibilities", e.target.value)}
+//             />
+
+//             {/* <button> Add more </button> */}
+
+//             </div>
+//   ))}
+
+//         </FormWrapper>
+//         </>
+//     )
+// }
+
 
 
